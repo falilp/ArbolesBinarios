dcfc95da906570272b8c1c79b8da7a34b4b7110b
falilp <falilealp@gmail.com>
2023-07-22 19:39:42 +0200

    Primer ejercicio

--- Ejercicios1.cpp
+++ Ejercicios1.cpp
@@ -1,3 +1,22 @@
 #include "abin.hpp"
 #include <iostream>
 
+//Implementa un subprograma que calcule el número de nodos de un árbol binario
+
+template <typename T>
+int ContarNodos(const Abin<T>& A, const typename Abin<T>::nodo n){
+    if(n == A.NODO_NULO){
+        return 0;
+    }else{
+        return 1 + ContarNodos(A,A.hijoIzqdo(n)) + ContarNodos(A,A.hijoDrcho(n));
+    }
+}
+
+template <typename T>
+int NumeroNodos(const Abin<T>& A){
+    if(A.arbolVacio() || (A.raiz() == A.NODO_NULO)){
+        return 0;
+    }else{
+        return ContarNodos(A,A.raiz());
+    }
+}
