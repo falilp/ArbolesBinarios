b2b49c521066a292f7d196e4ad8f9b4f8c511b1e
falilp <falilealp@gmail.com>
2023-07-22 19:59:49 +0200

    Segundo ejercicio

--- Ejercicios1.cpp
+++ Ejercicios1.cpp
@@ -1,7 +1,7 @@
 #include "abin.hpp"
 #include <iostream>
 
-//Implementa un subprograma que calcule el número de nodos de un árbol binario
+//Ejercicio 1:Implementa un subprograma que calcule el número de nodos de un árbol binario
 
 template <typename T>
 int ContarNodos(const Abin<T>& A, const typename Abin<T>::nodo n){
@@ -18,5 +18,30 @@ int NumeroNodos(const Abin<T>& A){
         return 0;
     }else{
         return ContarNodos(A,A.raiz());
+    }
+}
+
+//Ejercicio 2:Implementa un subprograma que calcule la altura de un árbol binario
+
+template <typename T>
+const T Maximo(const T& V1,const T& V2){
+    return (V1 > V2 ? a : b); 
+}
+
+template <typename T>
+int AlturaRecursiva(const Abin<T>& A,const typename Abin<T>::nodo n){
+    if(n == A.NODO_NULO){
+        return 0;
+    }else{
+        else 1 + Maximo();
+    }
+}
+
+template <typename T>
+int AlturaArbol(const Abin<T>& A){
+    if(A.arbolVacio() || (A.raiz() == A.NODO_NULO)){
+        return 0;
+    }else{
+        return AlturaRecursiva(A,A.raiz());
     }
 }
